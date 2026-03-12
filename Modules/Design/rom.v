0c6ae8071b627c3eb634efc31c4a098678278616
André Bezerra <andrefrbezerra@gmail.com>
2026-03-12 18:30:58 -0300

    Fix assignment operator for coeff in rom module

--- Modules/Design/rom.v
+++ Modules/Design/rom.v
@@ -1,11 +1,11 @@
 //a ROM lê automaticamente o arquivo na simulação
 
 module rom #(
-    parameter NUM_TAPS   = 8,
-    parameter COEFF_WIDTH = 8,
-    parameter FILE_NAME  = "fir_coeffs.mem"
+    parameter   NUM_TAPS   = 8,
+                COEFF_WIDTH = 8,
+                FILE_NAME  = "fir_coeffs.mem"
 )(
-    input  wire [$clog2(NUM_TAPS)-1:0] addr,
+    input    [$clog2(NUM_TAPS)-1:0] addr,
     output reg  signed [COEFF_WIDTH-1:0] coeff
 );
 
@@ -19,7 +19,7 @@ module rom #(
 
     // Leitura combinacional
     always @(*) begin
-        coeff = memory[addr];
+        coeff <= memory[addr];
     end
 
 endmodule
